ecb267b302ab6239efc76985bcf33cf1fc458efa
donkey96 <noire964@gmail.com>
2019-07-06 16:16:53 +0900

    WIP message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,25 @@
 $(function(){
+  function buildHTML(message) {
+    var image = ""
+    message.image ? image = `<img src="${message.image}">` : image = ""
+    var html = `<div class="message">
+                  <div class="message__upper-info">
+                    <div class="message__upper-info__talker">
+                      ${message.user_name}
+                    </div>
+                    <div class="message__upper-info__date">
+                      ${message.date}
+                    </div>
+                  </div>
+                  <div class="message__text">
+                    <p class="message__text__body">
+                      ${message.body}
+                    </p>
+                      ${image}
+                  </div>
+                </div>`
+    return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +31,10 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
     })
   })
 })
