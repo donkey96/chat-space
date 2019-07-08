dbe9a297efa96bdee1786ef23dcd3440fe025250
donkey96 <noire964@gmail.com>
2019-07-08 18:29:31 +0900

    Update index.js

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,4 +1,4 @@
-$(function () {
+$(document).on('turbolinks:load', function () {
 
 var search_list = $("#user-search-result");
 
@@ -39,4 +39,27 @@ function appendUser(user) {
       alert('ユーザー検索に失敗しました');
     })
   });
+
+  var search_list_add = $("#chat-group-users");
+
+  function appendUserAdd(user_name, user_id) {
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${ user_id }'>
+                  <p class='chat-group-user__name'>${ user_name }</p>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                </div>`
+      search_list_add.append(html);
+  }
+
+    $(document).on("click", ".chat-group-user__btn--add",function () {
+    $('#chat-group-users').val();
+      var user_id = $(this).data('user-id');
+      var user_name = $(this).data('user-name');
+      appendUserAdd(user_name, user_id);
+      $(this).parent().remove();
+    });
+    
+    $(document).on("click", ".user-search-remove", function () {
+      $(this).parent().remove();
+    });
 });
