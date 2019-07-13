8986e8bfaafb0d192ac28693311e35bdece50753
donkey96 <noire964@gmail.com>
2019-07-13 17:21:45 +0900

    Update index.js

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -44,8 +44,8 @@ function appendUser(user) {
   var search_list_add = $("#chat-group-users");
 
   function appendUserAdd(user_name, user_id) {
-    var html = `<div class='chat-group-user clearfix js-chat-member' id='${user_id}'>
-                  <input name='chat_group[user_ids][]' type='hidden' value='${ user_id }'>
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-ids'>
+                  <input name='group[user_ids][]' type='hidden' value='${ user_id }'>
                   <p class='chat-group-user__name'>${ user_name }</p>
                   <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
                 </div>`
